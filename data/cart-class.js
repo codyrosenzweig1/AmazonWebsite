740c2ab21f81d3a821cd2b10f15b53abbe5ac971
CodyRosenzweig <crose008@outlook.com>
2024-09-25 22:02:46 +1000

    Convert products into classes

--- data/cart-class.js
+++ data/cart-class.js
@@ -81,6 +81,6 @@ class Cart {
 const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
 
-console.log(cart);
-console.log(businessCart);
-console.log(businessCart instanceof Cart);
+//console.log(cart);
+//console.log(businessCart);
+//console.log(businessCart instanceof Cart);
